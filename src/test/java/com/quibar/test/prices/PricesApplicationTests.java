fc37a89017b9581628275f3bd3db7a447282ab5e
unknown <mquibar@yahoo.com.ar>
2022-01-12 17:29:51 -0300

    commit inicial creacion de proyecto

---
+++ src/test/java/com/quibar/test/prices/PricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quibar.test.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
