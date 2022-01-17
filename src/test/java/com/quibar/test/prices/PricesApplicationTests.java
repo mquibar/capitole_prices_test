5f05beab189219174d3ca03506a3dbd475c126f0
unknown <mquibar@yahoo.com.ar>
2022-01-17 16:18:54 -0300

    ContextLoads  test

--- src/test/java/com/quibar/test/prices/PricesApplicationTests.java
+++ src/test/java/com/quibar/test/prices/PricesApplicationTests.java
@@ -1,13 +1,25 @@
 package com.quibar.test.prices;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.quibar.test.prices.infra.api.PriceController;
 
 @SpringBootTest
 class PricesApplicationTests {
 
+	@Autowired
+	PriceController controller;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertNotNull(context);
+		assertNotNull(controller);
 	}
+	
 
 }
